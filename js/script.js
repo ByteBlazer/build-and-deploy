5f47c731839e2525db890ac48a9a26833a6c57fa
Sujith George <sujithgeorgeinfy@gmail.com>
2022-08-22 00:30:53 +0530

    aa

--- js/script.js
+++ js/script.js
@@ -25,7 +25,8 @@ module.exports = ({
   sisterApp
 }) => {
 
-  console.log("#################################################################"+JSON.stringify(context));
+  //console.log("#################################################################"+JSON.stringify(context));
+  
   //Lifted from Google. Don't bother how it works. Just hashes a string and return a positive number.
   const hash = (str) => {
     let arr = str.split("");
