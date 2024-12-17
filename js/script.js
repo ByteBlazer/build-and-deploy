8cc46c3f349d96b2c7ed7f0357e4d2962038e670
Sujith George <sujith@byteblazer.com>
2024-12-17 14:37:45 +0530

    ff

--- js/script.js
+++ js/script.js
@@ -439,6 +439,8 @@ module.exports = ({
 
   const prefixForMobileAppName = env == envNameForProduction ? "" : env + " - ";
 
+  console.log("namespace:" + namespace.substring(0, 12));
+
   const resultObj = {
     env,
     applicationName,
